b9639faf33258d54cd086ea5e5e7137fb0a5716a
J.Valentine <jackvalentine@jackarchive.com>
2017-02-09 01:30:44 +0900

    Add blog post model
    - Add blog post model
    - Add README.MD

---
+++ README.md
@@ -0,0 +1,13 @@
+# Django Study Project 
+ 이 저장소는 장고를 공부하기 위헤 만든 곳입니다.
+ 
+## 모델 구조
+
+### Post 구조
+|칼럼명|용도|
+|---|---|
+|`author`|작성자|
+|`title`|Post 제목|
+|`text`|Post 내용|
+|`publish`|출판 여부|
+|`created_date`|작성 날짜|
--- django_study/jacksblog/models.py
+++ django_study/jacksblog/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=1024)
+    text = models.TextField()
+    publish = models.CharField(max_length=1)
+    created_date = models.DateTimeField(
+        blank=True, null=True
+    )
