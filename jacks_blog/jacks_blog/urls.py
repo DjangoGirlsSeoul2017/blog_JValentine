60f16f4f3f7506041a20503f22f125a97d2af352
J.Valentine <jackvalentine@jackarchive.com>
2017-02-18 03:05:07 +0900

    프로젝트 재생성 블로그 앱 재 구축 git ignore 변경

--- jacks_blog/jacks_blog/urls.py
+++ jacks_blog/jacks_blog/urls.py
@@ -1,4 +1,4 @@
-"""django_study URL Configuration
+"""jacks_blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- jacks_blog/jacks_blog/wsgi.py
+++ jacks_blog/jacks_blog/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_study project.
+WSGI config for jacks_blog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_study.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jacks_blog.settings")
 
 application = get_wsgi_application()
