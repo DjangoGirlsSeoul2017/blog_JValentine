665dd9317c4722a68bacb179e7e9861547ab05f7
Jack <jackvalentine@jackarchive.com>
2017-03-19 17:57:07 +0900

    index 페이지 수정
    - index 페이지에 posts 를 이용해 간략 보기 하는 부분 추가
    - views 에 index post 간략 조회 부분 추가

--- jacks_blog/blog/views.py
+++ jacks_blog/blog/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'blog/index.html', {})
+    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    simple_posts = Post.objects.order_by('published_date')[:5]
+
+    return render(request, 'blog/index.html', {'simple_posts': simple_posts})
+
+def post_list(request):
+    post_list = Post.objects.order_by('published_date')
+    return render(request, 'blog/index.html', {'posts': post_list})
+
+def post_view(request):
+    posts = Post.objects.order_by('published_date')[:5]
+    return render(request, 'blog/index.html', {'posts': posts})
